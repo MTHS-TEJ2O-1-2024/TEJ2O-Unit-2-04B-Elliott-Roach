ce47e51f6c64fd78282783222b134ff4ac700743
Elliott-Roach <elliott.roach@mths.ca>
2024-09-19 11:27:12 -0400

    Changed wording and added happy

--- main.ts
+++ main.ts
@@ -12,7 +12,9 @@ basic.pause(1000)
 
 input.onButtonPressed(Button.A, function(){
     temperature = input.temperature()
-    basic.showString('the temperature is')
-    basic.showNumber(temperature)
+    basic.showString('the temperature is:')
+    basic.showNumber(temperature )
+    basic.showString('C.')
+    basic.showIcon(IconNames.Happy)
 })
 
