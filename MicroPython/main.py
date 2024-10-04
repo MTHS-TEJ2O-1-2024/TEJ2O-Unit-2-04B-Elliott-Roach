3363744fc97fee8edf515471757212a30f220005
Elliott-Roach <elliott.roach@mths.ca>
2024-10-04 14:58:15 +0000

    varubul changed

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -6,14 +6,14 @@ This module is a Micro:bit MicroPython program dose: takes tempiture
 
 from microbit import *
 
-temperature_in_celseis = int(0)
+temperature_in_celseis = int(-1)
 
 display.clear()
 sleep(1000)
 
 while True:
     if button_a.is_pressed():
-        temperature_in_celseis = temperature()
+        temperature_in_celseis = temperature
         display.scroll("the temperature is:")
         display.show(temperature_in_celseis)
         display.scroll("C.")
